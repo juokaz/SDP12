1c1ae277e419fa8dde50d8ddcf0a7bd9e9cc1096
Juozas Kaziukenas <juozas@juokaz.com>
2011-02-20 13:25:56 +0000

    Abstract moveToPoint method
    git-svn-id: http://svn3.xp-dev.com/svn/SDP12@37 a999eb98-acfe-4d63-a2f3-c0b6aa0b45c9

--- Movement/Server/src/main/strategy/AbstractStrategy.java
+++ Movement/Server/src/main/strategy/AbstractStrategy.java
@@ -2,6 +2,8 @@ package main.strategy;
 
 import main.Strategy;
 import main.Executor;
+import main.data.Point;
+import main.data.Robot;
 
 public abstract class AbstractStrategy implements Strategy {
 
@@ -9,5 +11,45 @@ public abstract class AbstractStrategy implements Strategy {
 
 	public void setExecutor(Executor executor) {
 		this.executor  = executor;
+	}
+	
+	/**
+	 * Move robot from robotX and robotY to a Point position in a pitch
+	 * 
+	 * Point can be a Ball, Pitch or other Robot
+	 * 
+	 * @param robot
+	 * @param point
+	 */
+	protected void moveToPoint(Robot robot, Point point) {
+		
+		double dirAngle = -1*Math.atan2(point.getY()-robot.getY(), point.getX()-robot.getX());
+		double dy = robot.getY() - point.getY();
+		double dx = robot.getX() - point.getX();
+		double distance = Math.sqrt(dx*dx + dy*dy);
+		System.out.println(getClass().getSimpleName() + "::Distance " + distance);
+		
+		dirAngle = Math.toDegrees(dirAngle);
+		System.out.println(getClass().getSimpleName() + "::dirAngle " + dirAngle);
+		System.out.println(getClass().getSimpleName() + "::dAngle " + Math.abs(dirAngle - robot.getT()));
+		int a=1;
+		int b=1;
+		if(dirAngle > robot.getT()){
+			//System.out.println("Turning left");
+			a=-1;
+			b=1;
+		} 
+		if (dirAngle < robot.getT())
+		{
+			//System.out.println("Turning right");
+			a=1;
+			b=-1;
+			
+		}
+		if(Math.abs(dirAngle -robot.getT())<30){
+			a = (int) (1*distance)/35;
+			b = (int) (1*distance)/35;		
+		}
+		executor.rotateWheels(a*50, b*50);
 	}
 }
--- Movement/Server/src/main/strategy/GoToBall.java
+++ Movement/Server/src/main/strategy/GoToBall.java
@@ -18,57 +18,10 @@ public class GoToBall extends AbstractStrategy implements Strategy {
 	
 	@Override
 	public void updateLocation(Location data) {
-		// TODO Auto-generated method stub
-		
-		/**
-		 * Get position of ball, robot, and move to ball
-		 */
-		
 		Robot robotA = data.getRobotB();
 		Ball ball = data.getBall();
-		
-
-		moveToBall(robotA, ball);
-		
-	}
 	
-	/**
-	 * Main method to move to the ball.
-	 * 
-	 * @param robot
-	 * @param ball
-	 */
-	public void moveToBall(Robot robot, Ball ball) {
-		
-		double dirAngle = -1*Math.atan2(ball.getY()-robot.getY(), ball.getX()-robot.getX());
-		double dy = robot.getY() - ball.getY();
-		double dx = robot.getX() - ball.getX();
-		double distance = Math.sqrt(dx*dx + dy*dy);
-		System.out.println("CopyOfBasicStrategy::Distance " + distance);
-		
-		dirAngle = Math.toDegrees(dirAngle);
-		System.out.println("CopyOfBasicStrategy::dirAngle " + dirAngle);
-		System.out.println("CopyOfBasicStrategy::dAngle " + Math.abs(dirAngle - robot.getT()));
-		int a=1;
-		int b=1;
-		if(dirAngle > robot.getT()){
-			//System.out.println("Turning left");
-			a=-1;
-			b=1;
-		} 
-		if (dirAngle < robot.getT())
-		{
-			//System.out.println("Turning right");
-			a=1;
-			b=-1;
-			
-		}
-		if(Math.abs(dirAngle -robot.getT())<30){
-			a = (int) (1*distance)/35;
-			b = (int) (1*distance)/35;		
-		}
-		executor.rotateWheels(a*50, b*50);
-		
+		moveToPoint(robotA, ball);
 	}
 	
 	public void setGap(int newGap) {
