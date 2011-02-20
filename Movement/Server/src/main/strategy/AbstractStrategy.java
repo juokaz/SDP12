7cf2efba043237b5f201ebaceae73da5ea02a343
Biser Hong <biserhong@gmail.com>
2011-02-20 14:51:54 +0000

    Drawables in AbstractStrategy.java fixed
    git-svn-id: http://svn3.xp-dev.com/svn/SDP12@44 a999eb98-acfe-4d63-a2f3-c0b6aa0b45c9

--- Movement/Server/src/main/strategy/AbstractStrategy.java
+++ Movement/Server/src/main/strategy/AbstractStrategy.java
@@ -64,13 +64,16 @@ public abstract class AbstractStrategy implements Strategy {
 				b = (int) (1*distance)/35;		
 			}
 			executor.rotateWheels(a*50, b*50);
+			
+			ArrayList<Drawable> drawables = new ArrayList<Drawable>();
+			drawables.add(new Drawable(Drawable.LABEL, "Distance: " + distance, 50, 30, Color.WHITE));
+			drawables.add(new Drawable(Drawable.LABEL, "dirAngle: " + dirAngle, 50, 50, Color.WHITE));
+			drawables.add(new Drawable(Drawable.LABEL, "robotAngle: " + robot.getT(), 50, 70, Color.WHITE));
+			
+			executor.setDrawables(drawables);
+			
 		}
 		
-		ArrayList<Drawable> drawables = new ArrayList<Drawable>();
-		drawables.add(new Drawable(Drawable.LABEL, "Distance: " + distance, 50, 30, Color.WHITE));
-		drawables.add(new Drawable(Drawable.LABEL, "dirAngle: " + dirAngle, 50, 50, Color.WHITE));
-		drawables.add(new Drawable(Drawable.LABEL, "robotAngle: " + robot.getT(), 50, 70, Color.WHITE));
 		
-		executor.setDrawables(drawables);
 	}
-}
+
