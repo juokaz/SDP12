76b8d23134b93d194ecc8af6e780032a0f1c7e4a
Marc Howarth <marc.howarth@gmail.com>
2011-03-01 16:22:54 +0000

    takepenalty2 added - Forgot to add this strategy to my previous commit
    git-svn-id: http://svn3.xp-dev.com/svn/SDP12@162 a999eb98-acfe-4d63-a2f3-c0b6aa0b45c9

---
+++ Movement/Server/src/main/strategy/TakePenalty2.java
@@ -0,0 +1,22 @@
+package main.strategy;
+
+import main.Strategy;
+import main.data.Location;
+
+public class TakePenalty2 extends AbstractStrategy implements Strategy {
+
+	private boolean hasTaken = false;
+	
+	@Override
+	public void updateLocation(Location data) {
+		
+		if ( hasTaken == false )
+			takePenalty();
+		
+	}
+	
+	protected void takePenalty() {
+		executor.takePenalty();
+		hasTaken = true;
+	}
+}
